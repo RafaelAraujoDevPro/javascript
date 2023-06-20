aea7f57f7cc6b22fe9cdb603dcbfaf67ff29d0cc
RafaelAraujoDevPro <137007445+RafaelAraujoDevPro@users.noreply.github.com>
2023-06-19 22:32:53 -0300

    class

--- exercícios/exercicios/orientacaoObjeto.js
+++ exercícios/exercicios/orientacaoObjeto.js
@@ -29,3 +29,22 @@ let pessoa2 = new Pessoa('Raylane','555 555 555',"Boa Vista-RR");
 pessoa2._mostrar();
 let pessoa3 = new Pessoa('Racklane','555 555 444','Boa Vista-RR');
 pessoa3._mostrar();
+
+/* ======= */
+
+class mamiferos {
+    constructor(especie,nome,idade) {
+        this.especie = especie
+        this.nome = nome
+        this.idade = idade
+        this.glandulamamaria = true
+    }
+    incrementaridade(){
+        this.idade++
+    }
+}
+const zeca = new mamiferos('zebra','zeca',4);
+zeca.incrementaridade()
+console.log(zeca);
+const lion = new mamiferos('leão','lion',7);
+lion.incrementaridade()
